1d8c596a88849bcb6a6c589c80c0b8d2e0e9eed6
NeoSwitch <Booklord34@gmail.com>
2021-04-13 21:41:54 +0700

    formating

--- S2_SDA_tugasAsdos1/tugasAsdos2.cpp
+++ S2_SDA_tugasAsdos1/tugasAsdos2.cpp
@@ -12,13 +12,13 @@ void alg (int n){
         int suku1=1,suku2=1,suku3=2;
         unsigned long long int next;
         for (int i = 3; i < n; i++){
-        suku1 = suku1 % MOD;
-        suku2 = suku2 % MOD;
-        suku3 = suku3 % MOD;
-        next=suku1 + suku2 + suku3;
-        suku1=suku2;
-        suku2=suku3;
-        suku3=next;
+            suku1 = suku1 % MOD;
+            suku2 = suku2 % MOD;
+            suku3 = suku3 % MOD;
+            next=suku1 + suku2 + suku3;
+            suku1=suku2;
+            suku2=suku3;
+            suku3=next;
         }
         next = next % MOD;
         cout << next;
