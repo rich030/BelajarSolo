89ba0941818731af70107c5de2b344b28a2d5fec
NeoSwitch <Booklord34@gmail.com>
2021-11-11 17:20:13 +0700

    Initial commit

---
+++ S3_PBO_5Thread/Thread.cpp
@@ -0,0 +1,38 @@
+#include "Thread.h"
+
+
+Thread :: Thread() {
+    pThread = NULL;
+    stopped = false;
+}
+
+Thread :: ~Thread() {
+    delete pThread;
+    pThread = NULL;
+}
+
+void Thread ::run() {
+    while ( ! stopped ) {
+        cout << "\n" << this_thread :: get_id() << endl;
+        this_thread :: sleep_for ( 1s );
+    }
+    cout << "\nThread " << this_thread :: get_id()
+    << " stopped as requested." << endl;
+    return;
+}
+
+void Thread :: stop() {
+    stopped = true;
+}
+
+void Thread :: start() {
+    pThread = new thread( &Thread :: run, this );
+}
+
+void Thread :: join() {
+    pThread -> join();
+}
+
+void Thread :: detach() {
+    pThread -> detach();
+}
